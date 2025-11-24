8d3fdd1451f05bbabe8f18a5cf39fb55d8dfbed2
MarynaPrykhodko <marpry4@alu.edu.gva.es>
2025-11-24 10:43:04 +0100

    Función Fibonacci

---
+++ Matematicas.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Alumno Maryna de 1 DAM");
+public class Matematicas
+
+{
+
+public static int Fibonacci(int n)
+
+{
+
+if (n <= 1) return n;
+
+return Fibonacci(n - 1) + Fibonacci(n - 2);
+
+}
+
+}
